78880fa883672e471862815a36e60b7a694bda37
Roman Melnychuk <roma27753@gmail.com>
2026-09-21 00:33:10 +0300

    Add domain models: ConferenceRoom, Service, Booking

---
+++ ConferenceRoomBooking.Api/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace ConferenceRoomBooking.Api.Models;
+
+public class Booking
+{
+	public int Id { get; set; }
+	public int RoomId { get; set; }
+	public ConferenceRoom Room { get; set; } = null!;
+	public DateTime StartTime { get; set; }
+	public DateTime EndTime { get; set; }
+	public decimal TotalPrice { get; set; }
+	public ICollection<Service> Services { get; set; } = new List<Service>();
+
+}
---
+++ ConferenceRoomBooking.Api/Models/ConferenceRoom.cs
@@ -0,0 +1,11 @@
+﻿namespace ConferenceRoomBooking.Api.Models;
+
+public class ConferenceRoom
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+    public decimal BasePricePerHour { get; set; }
+    public ICollection<Service> Services { get; set; } = new List<Service>();
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
